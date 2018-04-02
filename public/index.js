7da3820d2a43aa85f650070a610cee919b97f674
Nishant <nishantnischaya@gmail.com>
2018-04-02 23:46:26 +0530

    Update April 2, 2018

--- public/index.js
+++ public/index.js
@@ -10,13 +10,34 @@ $(window).resize(() =>{
 function adjust_size() {  
     canvas.width = window.innerWidth - 5;
     canvas.height = window.innerHeight - 5;
+    refresh();
+}
 
+function refresh(x){  
     var dimensions = {
         width: canvas.width,
         height: canvas.height,
         wsize: canvas.width * 0.1,
         hsize: canvas.height * 0.01
     }
-    context.rect((dimensions.width / 2), dimensions.height - dimensions.hsize, dimensions.wsize, dimensions.hsize);
+    context.rect((dimensions.width / 2) - dimensions.wsize / 2 + x, dimensions.height - dimensions.hsize, dimensions.wsize, dimensions.hsize);
     context.fill();
 }
+
+var x = 0;
+
+function animate() {
+    requestAnimationFrame(animate);
+    context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    document.addEventListener('keydown', (event) => {
+        const keyName = event.key;
+        if(keyName == 'ArrowRight'){
+            x++;
+        } else if (keyName == 'ArrowLeft'){
+            x--;
+        }
+    });
+    refresh(x);
+}
+
+animate();
